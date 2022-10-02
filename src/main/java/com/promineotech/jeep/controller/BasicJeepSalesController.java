a8ac78956b69a4995e75d24406ead141b18b0acc
jadegite <101847100+jadegite@users.noreply.github.com>
2022-10-02 15:23:40 -0400

    Week-14 update

--- src/main/java/com/promineotech/jeep/controller/BasicJeepSalesController.java
+++ src/main/java/com/promineotech/jeep/controller/BasicJeepSalesController.java
@@ -4,19 +4,27 @@
 package com.promineotech.jeep.controller;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
+import com.promineotech.jeep.service.JeepSalesService;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author USER
  *
  */
 @RestController
+@Slf4j
 public class BasicJeepSalesController implements JeepSalesController {
+  
+  @Autowired
+  private JeepSalesService jeepSalesService;
 
   public List<Jeep> fetchJeeps(String model, String trim) {
-    return null;
+    log.info("model={}, trim={}", model, trim);
+    return jeepSalesService.fetchJeeps(model, trim);
   }
 
   public List<Jeep> fetchJeeps(JeepModel model, String trim) {
@@ -24,4 +32,9 @@ public class BasicJeepSalesController implements JeepSalesController {
     return null;
   }
 
+//  public List<Jeep> fetchJeeps(JeepModel model, String trim) {
+//    // TODO Auto-generated method stub
+//    return null;
+//  }
+//
 }
